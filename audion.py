6bf69d23050953924b3d6aad40e6257320349520
DaniAguilarGarcia <41087691+DaniAguilarGarcia@users.noreply.github.com>
2020-02-02 10:45:59 -0500

    Update audion.py

--- audion.py
+++ audion.py
@@ -6,11 +6,23 @@ from google.cloud.speech_v1 import enums
 from google.cloud.speech_v1 import types
 client = speech_v1.SpeechClient()
 
-encoding = enums.RecognitionConfig.AudioEncoding.FLAC
-sample_rate_hertz = 44100
-language_code = 'en-US'
-config = {'encoding': encoding, 'sample_rate_hertz': sample_rate_hertz, 'language_code': language_code}
-uri = 'gs://bucket_name/file_name.flac'
-audio = {'uri': uri}
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'resources',
+    'audio.raw')
 
+# Loads the audio into memory
+with io.open(file_name, 'rb') as audio_file:
+    content = audio_file.read()
+    audio = types.RecognitionAudio(content=content)
+
+config = types.RecognitionConfig(
+    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code='en-US')
+
+# Detects speech in the audio file
 response = client.recognize(config, audio)
+
+for result in response.results:
+    print('Transcript: {}'.format(result.alternatives[0].transcript))
