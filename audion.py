d821421bd394f371b97afc1ac8c8bb44d678e3a9
DaniAguilarGarcia <41087691+DaniAguilarGarcia@users.noreply.github.com>
2020-02-02 10:18:52 -0500

    Update audion.py

--- audion.py
+++ audion.py
@@ -1,1 +1,16 @@
-//TBD
+##code using google cloud API, open source https://pypi.org/project/google-cloud-speech/ 
+
+
+from google.cloud import speech_v1 as speech
+from google.cloud.speech_v1 import enums
+from google.cloud.speech_v1 import types
+client = speech_v1.SpeechClient()
+
+encoding = enums.RecognitionConfig.AudioEncoding.FLAC
+sample_rate_hertz = 44100
+language_code = 'en-US'
+config = {'encoding': encoding, 'sample_rate_hertz': sample_rate_hertz, 'language_code': language_code}
+uri = 'gs://bucket_name/file_name.flac'
+audio = {'uri': uri}
+
+response = client.recognize(config, audio)
